7cf51c9aeacd8101c0fc88ce5ce4e9b2a8249116
waleed elshafey <145357749+waleed-e@users.noreply.github.com>
2024-06-25 15:09:32 +0300

    first commit

---
+++ pointers.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <cstring>
+#include <conio.h>
+using namespace std;
+class Student{
+	int  mark = 0;
+	int age = 0;
+	string name = "unKnown";
+public:
+	
+	void setValues() {
+		
+
+		cout << "Enter Your mark : " << mark << "\n";
+		cin >> mark;
+		
+		
+	}
+	friend int Avg(Student ob[],int size);
+};
+
+int Avg(Student ob[],int size) {
+	int sum = 0;
+	for (int i = 0; i < size; i++) {
+		 sum += ob[i].mark;
+	}
+	return sum / size;
+}
+
+int main(){
+	Student ob[5];
+
+	for (int i = 0; i < 5; i++) {
+		ob[i].setValues();
+	}
+
+	int agv = Avg(ob, 5);
+	cout << agv;
+}
